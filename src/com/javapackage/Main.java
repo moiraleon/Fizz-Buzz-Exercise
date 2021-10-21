4b6c299f0a5923b20a3a2c5c6f68d22404b58b1a
Moira Leon <moira.leon2@gmail.com>
2021-10-21 16:22:35 -0600

    pseudo code

--- src/com/javapackage/Main.java
+++ src/com/javapackage/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //divisible by 5 fizz
+        //divisible by 3 buzz
+        //divisible by 3 and 5 fizzbuzz
+        //else return the number
+        int number = 3;
+        switch(number){
+            case (number/3) % ==0:
+                System.out.println("Buzz");
+                break;
+            case (number/5) %==0:
+                System.out.println("Fizz");
+                break;
+            default:
+                System.out.println(number);
+        }
     }
 }
