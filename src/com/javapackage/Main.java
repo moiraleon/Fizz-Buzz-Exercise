5e4405a935b12e19f20fcd5248969dd442b1f4c5
Moira Leon <moira.leon2@gmail.com>
2021-10-21 16:35:20 -0600

    if statement approach

--- src/com/javapackage/Main.java
+++ src/com/javapackage/Main.java
@@ -15,15 +15,34 @@ public class Main {
        int number = scanner.nextInt();
 
 
-        switch(number){
-            case (number/3) % ==0:
-                System.out.println("Buzz");
-                break;
-            case (number/5) %==0:
-                System.out.println("Fizz");
-                break;
-            default:
-                System.out.println(number);
+//        switch(number){
+//            case (number % 3 == 0):
+//                System.out.println("Buzz");
+//                break;
+//            case (number % 5 == 0):
+//                System.out.println("Fizz");
+//                break;
+//
+//            case (number % 5 ==0 && number % 3 ==0):
+//                System.out.println("FizzBuzz");
+//                break;
+//
+//            default:
+//                System.out.println(number);
+//        }
+
+        //with if else statement
+
+        if (number % 5 ==0 && number % 3 ==0) {
+            System.out.println("FizzBuzz");
         }
+            else if (number % 5 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (number % 3 == 0) {
+                System.out.println("Buzz");
+        }
+            else
+            System.out.println(number);
     }
 }
